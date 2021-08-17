79d09bc1f03465b8e03e916ea2fd215224d5fffc
Park Sung Min <sm.park@coint.co.kr>
2021-08-17 18:38:52 +0900

    Preparing for v1

---
+++ src/Evergreen/V1/Types.elm
@@ -0,0 +1,25 @@
+module Evergreen.V1.Types exposing (..)
+
+
+type alias FrontendModel =
+    {}
+
+
+type alias BackendModel =
+    {}
+
+
+type FrontendMsg
+    = NoOpFrontendMsg
+
+
+type ToBackend
+    = NoOpToBackend
+
+
+type BackendMsg
+    = NoOpBackendMsg
+
+
+type ToFrontend
+    = NoOpToFrontend
